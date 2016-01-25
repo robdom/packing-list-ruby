48ac9f6e9eba7f21ed27a5aac00f2b4455aacf05
Robert Domingo <robertdomingo@robert-domingos-macbook.local>
2016-01-25 12:06:08 -0500

    Added devise, factory-girl and ffaker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
-Capybara.javascript_driver = :webkit
+require 'capybara/webkit/matchers'
 require 'simple_bdd'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -28,6 +28,7 @@ require 'simple_bdd'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  Capybara.javascript_driver = :webkit
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
